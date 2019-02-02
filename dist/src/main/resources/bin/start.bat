2708bc693f0102ab843977b8882495c30b34eb19
dengdeng <dengyong.nju@outlook.com>
2019-02-02 15:18:52 +0000

    update se

--- dist/src/main/resources/bin/start.bat
+++ dist/src/main/resources/bin/start.bat
@@ -5,7 +5,7 @@ pushd %~dp0..
 set   SE_HOME=%CD%
 popd
 
-set CLASSPATH=%SE_HOME%/bin/*;%SE_HOME%/etc;%SE_HOME%/lib/*
+set CLASSPATH=%SE_HOME%/etc;%SE_HOME%/lib/*
 
 if not "%DEBUG%"=="" (
     set JAVA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=7878,server=y,suspend=y
--- start/src/main/java/com/greenbee/se/start/JettyMain.java
+++ start/src/main/java/com/greenbee/se/start/JettyMain.java
@@ -11,14 +11,17 @@ import java.net.URLClassLoader;
 import java.util.List;
 import java.util.Properties;
 
+import com.greenbee.commons.ClasspathLib;
+import com.greenbee.commons.FileLib;
+import com.greenbee.commons.ReflectionLib;
+import com.greenbee.commons.ResourceLoaderUtil;
+
 public class JettyMain {
     public static final String SYS_SE_HOME = "se.home";
     public static final String SYS_JETTY_HOME = "jetty.home";
     public static final String SYS_JETTY_BASE = "jetty.base";
     public static final String SE_PROPERTY_JETTYCLASS = "jetty.class";
     public static final String SE_PROPERTY_JETTYXML = "jetty.xml";
-    public static final String SE_PROPERTY_CLASSPATH = "classpath.root";
-    public static final String SE_PROPERTY_CLASSPATH_JETTY = "classpath.jetty";
     public static final String SE_HOME = "SE_HOME";
     public static final String SE_PROPERTIES = "se_conf.properties";
     private Properties settings_;
@@ -36,29 +39,7 @@ public class JettyMain {
         System.setProperty(SYS_JETTY_BASE, home_);
         System.setProperty(SYS_SE_HOME, home_);
         ClassLoader cl = JettyMain.class.getClassLoader();
-        settings_ = ResourceLoader.loadResourceAsProperties(cl, SE_PROPERTIES);
-    }
-
-    /**
-     * JVM class loader
-     * ROOT class loader
-     * JETTY class loader
-     */
-    protected void prepareClassLoader() throws Exception {
-        String rootClasspath = settings_.getProperty(SE_PROPERTY_CLASSPATH);
-        List<URL> rootUrls = ClasspathLib.getClasspathURLs(home_, rootClasspath);
-        String jettyClasspath = settings_.getProperty(SE_PROPERTY_CLASSPATH_JETTY);
-        List<URL> jettyUrls = ClasspathLib.getClasspathURLs(home_, jettyClasspath);
-        //prepare class loader
-        ClassLoader parent = JettyMain.class.getClassLoader();
-        ClassLoader cl = parent;
-        //consider case when they are both empty
-        if (rootUrls.size() > 0)
-            cl = new URLClassLoader(rootUrls.toArray(new URL[0]), cl);
-        if (jettyUrls.size() > 0)
-            cl = new URLClassLoader(jettyUrls.toArray(new URL[0]), cl);
-        if (cl != parent)
-            Thread.currentThread().setContextClassLoader(cl);
+        settings_ = ResourceLoaderUtil.loadProperties(cl, SE_PROPERTIES);
     }
 
     protected void setupLog4j() throws Exception {
@@ -70,7 +51,7 @@ public class JettyMain {
         ReflectionLib.invokeStatic(null, clazz, method, paramTypes, paramValues);
     }
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SuppressWarnings({ "rawtypes" })
     protected void startJettyServer() throws Exception {
         //like etc/jetty.xml,etc/jetty_extend.xml
         String jettyXML = settings_.getProperty(SE_PROPERTY_JETTYXML);
@@ -80,17 +61,15 @@ public class JettyMain {
             apaths.set(i, FileLib.join(home_, path));
         }
         String jettyClazz = settings_.getProperty(SE_PROPERTY_JETTYCLASS);
-        Class jettyClass = ResourceLoader.loadClass(jettyClazz);
         String[] args = apaths.toArray(new String[0]);
         Class[] paramTypes = new Class[] { args.getClass() };
         Object[] params = new Object[] { args };
-        Method method = jettyClass.getDeclaredMethod("main", paramTypes);
-        method.invoke((Object) null, params);
+        ReflectionLib.invokeStatic(JettyMain.class.getClassLoader(), jettyClazz,
+                                   "main", paramTypes, params);
     }
 
     public void run() throws Exception {
         init();
-        prepareClassLoader();
         setupLog4j();
         startJettyServer();
     }
