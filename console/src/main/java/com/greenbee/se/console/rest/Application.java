2bd8b15286e5a57ac90b5dd87cdec0e8721fd36f
dengdeng <dengyong.nju@outlook.com>
2019-02-03 04:57:58 +0000

    refine security

--- console/src/main/java/com/greenbee/se/console/rest/Application.java
+++ console/src/main/java/com/greenbee/se/console/rest/Application.java
@@ -1,16 +1,14 @@
 package com.greenbee.se.console.rest;
 
-//import javax.ws.rs.ApplicationPath;
-
+import org.apache.commons.lang3.ClassUtils;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
-//@ApplicationPath("/rest")
 public class Application extends ResourceConfig {
 
     public Application() {
         registerClasses(JacksonFeature.class);
-        packages("com.greenbee.se.console.ws");
+        packages(ClassUtils.getPackageName(this.getClass()));
     }
 
 }
